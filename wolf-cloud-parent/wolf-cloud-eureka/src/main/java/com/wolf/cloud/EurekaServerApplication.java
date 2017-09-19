6b96ce183e14a69c6f4e5a4e2c57d51e21b34384
WolfLB <sunboy_liaobin20@163.com>
2017-09-19 20:51:45 +0800

    创建spring cloud eureka 服务端

---
+++ wolf-cloud-parent/wolf-cloud-eureka/src/main/java/com/wolf/cloud/EurekaServerApplication.java
@@ -0,0 +1,19 @@
+package com.wolf.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Created by wolf on 17/9/19.
+ */
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+
+}
