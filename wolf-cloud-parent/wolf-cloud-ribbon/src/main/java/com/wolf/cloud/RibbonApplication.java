dc47f640c656605700bf680a6b1c496b37e0aca8
WolfLB <sunboy_liaobin20@163.com>
2017-09-21 20:09:23 +0800

    eureka client

---
+++ wolf-cloud-parent/wolf-cloud-ribbon/src/main/java/com/wolf/cloud/RibbonApplication.java
@@ -0,0 +1,28 @@
+package com.wolf.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * Created by wolf on 17/9/21.
+ */
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class RibbonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RibbonApplication.class, args);
+    }
+
+    @Bean
+    @LoadBalanced
+    RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
+
+}
---
+++ wolf-cloud-parent/wolf-cloud-user/src/main/java/com/wolf/cloud/UserApplication.java
@@ -0,0 +1,17 @@
+package com.wolf.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * Created by wolf on 17/9/21.
+ */
+
+@SpringBootApplication
+@EnableEurekaClient
+public class UserApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UserApplication.class, args);
+    }
+}
