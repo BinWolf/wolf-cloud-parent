9e79845ef8857b974bdd3891ad2127e2323b0188
WolfLB <sunboy_liaobin20@163.com>
2017-10-06 23:25:44 +0800

    config client

--- wolf-cloud-parent/wolf-cloud-user/src/main/java/com/wolf/cloud/user/controller/UserController.java
+++ wolf-cloud-parent/wolf-cloud-user/src/main/java/com/wolf/cloud/user/controller/UserController.java
@@ -1,10 +1,7 @@
 package com.wolf.cloud.user.controller;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Created by wolf on 17/9/22.
@@ -13,11 +10,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/user")
 public class UserController {
 
+    @Value("${author}")
+    private String author;
+
     @Value("${server.port}")
     String port;
 
     @RequestMapping("/{name}")
     public String sayHi(@PathVariable("name") String name) {
         return "Hi, " + name + "; port = " + port;
+    }
+
+    @GetMapping("getAuthor")
+    public String author() {
+        return this.author;
     }
 }
